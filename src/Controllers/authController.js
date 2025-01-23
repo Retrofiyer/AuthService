2e7cd90133383938f821e41b5632a70387fc7812
Sebastian <sebitas5225@gmail.com>
2025-01-22 21:13:28 -0500

    build: :fire: Removed all files except for README

--- src/Controllers/authController.js
+++
@@ -1,6 +0,0 @@
-function loginController(req, res) {
-  const { token, role } = req.authData;
-  res.status(200).json({ message: 'Login successful', token, role });
-}
-
-module.exports = { loginController };
--- src/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authMiddleware } = require('../Middlewares/authMiddleware');
-const { loginController } = require('../Controllers/authController');
-
-const router = express.Router();
-
-router.post('/login', authMiddleware, loginController);
-
-module.exports = router;

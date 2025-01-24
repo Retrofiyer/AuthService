0d26716d25d4b7ada944d973686b08c4ab465094
Sebastian <sebitas5225@gmail.com>
2025-01-24 15:02:31 -0500

    feat: :sparkles: Modified swagger.yaml and ci pipeline file

--- src/index.js
+++ src/index.js
@@ -4,33 +4,31 @@ const dotenv = require("dotenv")
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const fs = require('fs')
 const errorHandler = require('./Middlewares/errorMiddleware');
 const authRoutes = require('./Routes/authRoutes');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Load Swagger YAML file
-const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
+const swaggerDocument = YAML.load('./src/Docs/swagger.yaml');
 
-// Constant that validates in which branch the swagger will be found.
-const environment = process.env.NODE_ENV || 'dev';
-
-dotenv.config();
+// Dynamic Swagger control according to branch
+if (fs.existsSync('./src/Docs/swagger.yaml')) {
+    console.log('Swagger documentation enabled.');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} else {
+    console.log('Swagger documentation disabled.');
+}
 
 // Middleware
 app.use(errorHandler);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-// Dynamic Swagger control according to branch
-if (['dev', 'QA'].includes(environment)) {
-    console.log(`Swagger enabled in environment: ${environment}`);
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-} else {
-    console.log(`Swagger disabled in environment: ${environment}`);
-}
 
 // Routes
 app.use('/auth', authRoutes);

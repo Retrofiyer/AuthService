7c3fc121a25f797b5eadbdd1a453ade68efd68ef
Sebastian <sebitas5225@gmail.com>
2025-02-01 21:13:19 -0500

    feat: :sparkles: DB/connection/Changed const connection to const pool

--- src/Middlewares/security.js
+++ src/Middlewares/security.js
@@ -4,7 +4,7 @@ const rateLimit = require("express-rate-limit");
 
 // Configure CORS
 const corsOptions = {
-    origin: ["http://localhost"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
--- src/Models/authModel.js
+++ src/Models/authModel.js
@@ -2,8 +2,8 @@ const db = require('../DB/connection');
 
 async function authenticateUser(idVolunteer, ci) {
     try {
-        const query = 'SELECT idVolunteer, Role FROM user WHERE idVolunteer = ? AND C.I = ?';
-        const rows = await db.query(query, [idVolunteer, ci]);
+        const query = 'SELECT idVolunteer, Role FROM user WHERE idVolunteer = ? AND ci = ?';
+        const [rows] = await db.query(query, [idVolunteer, ci]);
         return rows.length > 0 ? rows[0] : null;
     } catch (error) {
         console.error('Error authenticating user:', error);

4471fc70cfed41c2ea11b73647c85898f7fc0386
Sebastian <sebitas5225@gmail.com>
2025-02-02 23:17:29 -0500

    feat: :sparkles: src/Middlewares/authMiddleware/Added validation ci

--- src/Middlewares/authMiddleware.js
+++ src/Middlewares/authMiddleware.js
@@ -8,6 +8,10 @@ async function authMiddleware(req, res, next) {
     try {
         const { idVolunteer, ci } = req.body;
 
+        if (!/^\d{10}$/.test(ci)) {
+            return res.status(400).json({ error: "The ID must have 10 digits" });
+          }
+
         if (!idVolunteer || !ci) {
             return res.status(400).json({ error: 'Missing idVolunteer or C.I.' });
         }
